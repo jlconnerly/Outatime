65d8eeb9421577a93b17725b2bcdede7a34c2f04
Jake Connerly <jlconnerly@gmail.com>
2019-07-24 14:24:12 -0500

    Initial Commit

---
+++ Outatime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Outatime
+//
+//  Created by Jake Connerly on 7/24/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
