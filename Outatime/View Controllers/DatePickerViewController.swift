a3814dfec91b282916e169f0898eeb5c6e25bf13
Jake Connerly <jlconnerly@gmail.com>
2019-07-24 14:50:24 -0500

    UI Setup Complete

---
+++ Outatime/View Controllers/DatePickerViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DatePickerViewController.swift
+//  Outatime
+//
+//  Created by Jake Connerly on 7/24/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Outatime/View Controllers/TimeCircuitsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TimeCircuitsViewController.swift
+//  Outatime
+//
+//  Created by Jake Connerly on 7/24/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+
+    @IBOutlet weak var destinationLabel: UILabel!
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    @IBOutlet weak var lastDepartedLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func travelBackButtonTapped(_ sender: UIButton) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
